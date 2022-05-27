0d072443b6680c448a11844bdafa54422f560614
JunHwan Kim <nefertiri972@gmail.com>
2022-05-28 07:46:21 +0900

    Change function component to class component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  const name = '리액트';
-  return (
-    <>
-      {/* JSX 내부에서 주석 사용 */}
-      <div
-        className="react"
-        // 시작 태그를 여러 줄로 작성하였을 때, 태그 안에서 주석 사용이 가능하다.
-      >
-        {name}
-      </div>
-      // 이런 주석은 페이지에 나타난다. /* 이런 주석은 페이지에 나타난다. */
-      {/* 주석 테스트 */}
-      <input />
-    </>
-  );
+class App extends Component {
+  render() {
+    const name = 'react';
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
