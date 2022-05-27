bf154ca91a92beeedbf7cb3223d3aed2d38a9413
JunHwan Kim <nefertiri972@gmail.com>
2022-05-28 00:11:17 +0900

    Add JSX test

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '리액트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* JSX 내부에서 주석 사용 */}
+      <div
+        className="react"
+        // 시작 태그를 여러 줄로 작성하였을 때, 태그 안에서 주석 사용이 가능하다.
+      >
+        {name}
+      </div>
+      // 이런 주석은 페이지에 나타난다. /* 이런 주석은 페이지에 나타난다. */
+      {/* 주석 테스트 */}
+      <input />
+    </>
   );
 }
 
