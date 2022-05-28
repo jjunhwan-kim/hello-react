9457b6b56f67f7ad3cc24eb3534b226e2c8066d5
JunHwan Kim <nefertiri972@gmail.com>
2022-05-28 11:47:21 +0900

    Add state of class component
    - 클래스형 컴포넌트에 state 추가
    - setState 메서드 호출 시 인자로 전달된 값만 바꾸어줌, fixedNumber는 바꾸지 않음

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent';
-import MyClassComponent from './MyClassComponent';
+//import MyComponent from './MyComponent';
+//import MyClassComponent from './MyClassComponent';
+import Counter from './Counter';
 
 class App extends Component {
   render() {
     return (
       <>
+        {/*       
         <MyComponent name={'react'} favoriteNumber={7}>
           {'3'}
         </MyComponent>
         <br />
 
         <MyClassComponent></MyClassComponent>
+        */}
+
+        <Counter></Counter>
       </>
     );
   }
---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props); // 생성자 사용시 반드시 호출해야 함
+    // state의 초기값 설정
+    this.state = {
+      number: 0,
+      fixedNumber: 0,
+    };
+  }
+
+  render() {
+    const { number, fixedNumber } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
