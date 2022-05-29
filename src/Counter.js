9e57bf15e055e2e45aff876b200bd3e4d473f071
JunHwan Kim <nefertiri972@gmail.com>
2022-05-29 22:44:52 +0900

    Add argument to setState method
    - setState의 두 번째 파라미터로 콜백 함수를 등록하면, setState로 값이 업데이트된 이후에 콜백이 호출됨
    - setState를 사용하여 값을 업데이트한 뒤 특정 작업을 하고싶을 때 사용

--- src/Counter.js
+++ src/Counter.js
@@ -30,6 +30,7 @@ class Counter extends Component {
             //this.setState({ number: this.state.number + 1 });
           }}
           */
+          /*
           onClick={() => {
             // setState 메서드에 함수를 전달하여 1씩 증가하는 문제 해결
             this.setState((prevState) => {
@@ -41,6 +42,14 @@ class Counter extends Component {
             this.setState((prevState) => ({
               number: prevState.number + 1,
             }));
+          }}
+          */
+          onClick={() => {
+            // setState의 두 번째 파라미터로 콜백 함수를 등록하면, setState로 값이 업데이트된 이후에 콜백이 호출됨
+            this.setState({ number: number + 1 }, () => {
+              console.log('방금 setState가 호출되었습니다.');
+              console.log(this.state);
+            });
           }}
         >
           +1
